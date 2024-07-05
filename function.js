38f8168d5be0fd8f1ad51c8be15adcb644d4e940
24lp3471@nest.edu.mn <24LP3471@G304-CC0117.local>
2024-07-05 12:21:04 +0800

    filter

---
+++ function.js
@@ -0,0 +1,385 @@
+// BODLOGO-1 өгөгдсөн гурвалжны приметрыг ол.
+
+// const primeter = (a, b, c) => {
+//   let p = a + b + c;
+
+//   return p;
+// };
+
+// const a = 5;
+// const b = 3;
+// const c = 4;
+
+// const result = primeter(a, b, c);
+
+// console.log("өгөгдсөн гурвалжны приметер =", result);
+
+// BODLOGO-2 бүхэл тооны нийлбэрийг ол
+
+// const buhel = (a, b, c, d, e) => {
+//   let N = a + b + c + d + e;
+//   const buhelToo = parseInt(N);
+//   return buhelToo;
+// };
+
+// const a = 1;
+// const b = 3;
+// const c = 5;
+// const d = 7;
+// const e = 8.7;
+
+// const result = buhel(a, b, c, d, e);
+// console.log("buhel toonii niilber", "=", result);
+
+// BODLOGO-3 өгөгдсөн тэгш өнцөгтийн талбай ба периметрийг ол. Урт=10; өргөн=5;
+
+// const talbaiPrimeter = (a, b) => {
+//   let talbai = a * b;
+//   let primeter = (a + b) * 2;
+
+//   return [talbai, primeter];
+// };
+
+// const a = 5;
+// const b = 10;
+
+// const result = talbaiPrimeter(a, b);
+
+// console.log("талбай =", result[0]);
+// console.log("периметр =", result[1]);
+
+// BODLOGO-4 өгөгдсөн 98 секундыг минут секунд болго.
+
+// const minutSec = (a) => {
+//   let sec = a % 60;
+//   let min = (a - sec) / 60;
+
+//   return [min, sec];
+// };
+
+// let a = 98;
+
+// const result = minutSec(a);
+
+// console.log("минут =", result[0]);
+// console.log("секунд =", result[1]);
+
+// BODLOGO-5 өгөгдсөн 1297 секундыг цаг, минут, секунд болго.
+
+// const tsagMinutSec = (a) => {
+//   let sec = a % 60;
+//   let min = 0;
+//   let hour = 0;
+
+//   min = parseInt((a - sec) / (60 * 60));
+//   hour = parseInt(a / (60 * 60));
+
+//   return [hour, min, sec];
+// };
+
+// let a = 3666;
+// const result = tsagMinutSec(a);
+
+// console.log("цаг =", result[0]);
+// console.log("минут =", result[1]);
+// console.log("секунд =", result[2]);
+
+// BODLOGO-6 өгөгдсөн 875 минут, 88 секундыг секунд рүү шилжүүл.
+
+// const secund = (min, sec) => {
+//   a = sec + min * 60;
+//   return a;
+// };
+
+// let min = 1;
+// let sec = 28;
+
+// const result = secund(min, sec);
+// console.log("секунд =", result);
+
+// BODLOGO-7 өгөгдсөн 83 цаг, 84 минут, 85 секундыг секунд рүү шилжүүл.
+
+// const secund = (hour, min, sec) => {
+//   a = sec + min * 60 + hour * 60 * 60;
+//   return a;
+// };
+
+// let hour = 1;
+// let min = 1;
+// let sec = 1;
+
+// const result = secund(hour, min, sec);
+// console.log("секунд =", result);
+
+// BODLOGO-8: өгөгдсөн 38 цагийг хоногт шилжүүл.
+
+// const honog = (a) => {
+//   let tsag = a % 24;
+//   let honog = (a - tsag) / 24;
+//   return [honog, tsag];
+// };
+
+// let a = 38;
+
+// result = honog(a);
+
+// console.log("хоног =", result[0]);
+// console.log("цаг =", result[1]);
+
+// BODLOGO-9: өгөгдсөн 4 хоног, 40 цагийг хоногт шилжүүл.
+
+// const honog = (ugsun_honog, ugsun_tsag) => {
+//   let tsag = ugsun_tsag % 24;
+//   let honog = (ugsun_tsag - tsag) / 24 + ugsun_honog;
+//   return [honog, tsag];
+// };
+
+// let ugsun_honog = 4;
+// let ugsun_tsag = 40;
+
+// result = honog(ugsun_honog, ugsun_tsag);
+
+// console.log("хоног =", result[0]);
+// console.log("цаг =", result[1]);
+
+// BODLOGO-10: өгөгдсөн 106 сарыг жил, сар руу шилжүүл.
+
+// const jilSar = (ugsun_sar) => {
+//   let sar = ugsun_sar % 12;
+//   let jil = (ugsun_sar - sar) / 12;
+//   return [jil, sar];
+// };
+
+// let ugsun_sar = 121;
+
+// result = jilSar(ugsun_sar);
+
+// console.log("жил =", result[0]);
+// console.log("сар =", result[1]);
+
+// BODLOGO-11: Өгөгдсөн 90 жил, 98 сарыг сард шилжүүл.
+
+// let ugsun_jil = 90;
+// let ugsun_sar = 98;
+// let sar = ugsun_jil * 12 + ugsun_sar;
+
+// BODLOGO-5: Өгөгдсөн 4 тооны багыг ол. (9, 3, 2, 8)
+
+// const a = 9;
+// const b = 23;
+// const c = 12;
+// const d = 38;
+// let minimum = a;
+
+// if (minimum > a) {
+//   minimum = a;
+// }
+// if (minimum > b) {
+//   minimum = b;
+// }
+// if (minimum > c) {
+//   minimum = c;
+// }
+// if (minimum > d) {
+//   minimum = d;
+// }
+
+// console.log("minimum =", minimum);
+
+// BODLOGO-6: Өгөгдсөн 4 тооны 5-аас бага тоонуудын үржвэрийг ол. (2, 9, 4, 3)
+
+// const a = 2;
+// const b = 9;
+// const c = 4;
+// const d = 3;
+// let urjver = 1;
+
+// if (a < 5) {
+//   urjver = urjver * a;
+// }
+// if (b < 5) {
+//   urjver = urjver * b;
+// }
+// if (c < 5) {
+//   urjver = urjver * c;
+// }
+// if (d < 5) {
+//   urjver = urjver * d;
+// }
+
+// console.log("5-аас бага тоонуудын үржвэр", "=", urjver);
+
+// BODLOGO-7: Өгөгдсөн 6 тооны 5-аас их тоонуудын үржвэрийг ол. (11, 9, 4, 3)
+
+// const a = 11;
+// const b = 9;
+// const c = 4;
+// const d = 3;
+// let urjver = 1;
+
+// if (a > 5) {
+//   urjver = urjver * a;
+// }
+// if (b > 5) {
+//   urjver = urjver * b;
+// }
+// if (c > 5) {
+//   urjver = urjver * c;
+// }
+// if (d > 5) {
+//   urjver = urjver * d;
+// }
+
+// console.log("5-аас их тоонуудын үржвэр", "=", urjver);
+
+// const fruits = (a, b, c) => {
+//   let ehnii = a[0];
+// };
+
+//Bodlogo:forEach.
+
+// const ehniiUseg = (arr) => {
+//   arr.forEach((el) => {
+//     console.log(el[0]);
+//   });
+
+//   return [];
+// };
+
+// const arr = ["apple", "banana", "cherry"];
+
+// const arr = ["white", "black", "green"];
+
+// const result = ehniiUseg(arr);
+
+// console.log(result);
+
+//Bodlogo: Sum.
+
+// const arr = [10, 20, 30, 40, 50, 5];
+
+// let sum = 0;
+// arr.forEach((el, ind) => {
+//   sum = sum + el;
+// });
+// console.log(sum);
+
+// Bodlogo: Sum_function;
+
+// const niilber = (arr) => {
+//   let sum = 0;
+//   arr.forEach((el) => {
+//     sum = sum + el;
+//   });
+//   return sum;
+// };
+
+// const arr = [22, 4452, 2456, 9887];
+
+// const arr = [20, 40, 50, 90];
+
+// const result = niilber(arr);
+
+// console.log("SUM", "=", result);
+
+//MAP, FILTER, FIND
+
+//Bodlogo-1: Kvadrat.
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const numbersKvadrat = numbers.map((item, index) => item * item);
+
+// console.log(numbersKvadrat);
+
+//Bodlogo-2: 20% nemeh
+
+// const prices = [100, 200, 300, 400];
+
+// const pricesNemegdel = prices.map((item, index) => item + item * 0.2);
+
+// console.log(pricesNemegdel);
+
+//Bodlogo-3: 10% hungulult
+
+// const prices = [100, 200, 300, 400];
+
+// const pricesHungulult = prices.map((item, index) => item - item * 0.1);
+
+// console.log(pricesHungulult);
+
+//Bodlogo-4: 3-iin too hed deh indeh deer baigaag ol.
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const numberGurav = numbers.map((item, index) => {
+//   if (item === 3) {
+//     console.log(index);
+//   }
+// });
+
+//Bodlogo-5: 3-aas baga toonuudiig ol.
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const numbersGurav = numbers.map((item, index) => {
+//   if (item < 3) {
+//     console.log(item);
+//   }
+// });
+
+//Bodlogo-6: Dund ni baigaa toog olj 10-aar urjuul.
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const numberDund = numbers.map(
+//   (item, index) => (newIndex = Math.floor(numbers.length / 2))
+// );
+// console.log(numbers[newIndex] * 10);
+
+//Bodlogo-7: Sondgoi toonuudiig ol.
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const filteredSondgoi = numbers.filter((item, index) => {
+//   if (item % 2 !== 0) {
+//     console.log("SondgoiToo", "=", item);
+//   }
+// });
+
+//Bodlogo-8: J usgeer ehelsen neriig olj butsaa.
+
+// const nameJ = ["John", "Jane", "James", "Alica", "Bod"];
+
+// const filtered = nameJ.filter((item) => item[0] === "J");
+
+// console.log(filtered);
+
+//Bodlogo-9: 345-aas ih bolon baga toonuudiig yalgaj 2 array butsaa.
+
+// const prices = [120, 344, 200, 323, 411, 405, 20, 40, 504, 993];
+
+// const pricesBaga = prices.filter((item, index) => {
+//   if (item < 345) {
+//     const itemBaga = item;
+//     return itemBaga;
+//   }
+// });
+
+// const pricesIh = prices.filter((item, index) => {
+//   if (item > 345) {
+//     const itemIh = item;
+
+//     return itemIh;
+//   }
+// });
+// console.log("345-aas Baga", "=", pricesBaga);
+// console.log("345-aas Ih", "=", pricesIh);
+
+//Bodlogo-10: 9 usegtei string-iig ol.
+
+const strings = ["apple", "banana", "cherry", "blueberry", "date"];
+
+const filtered = strings.filter((item) => item[0] === "b");
+
+console.log(filtered);
--- palindrom.js
+++ palindrom.js
@@ -1,13 +1,129 @@
 //Bodlogo-11.
 
-let input = 1234321;
-const str = input.toString();
-let isPal = true;
+// let input = 1234321;
+// const str = input.toString();
+// let isPal = true;
 
-for (let i = 0; i < str.length / 2; i++) {
-  if (str[i] !== str[str.length - 1 - i]) {
-    isPal = false;
-    break;
+// for (let i = 0; i < str.length / 2; i++) {
+//   if (str[i] !== str[str.length - 1 - i]) {
+//     isPal = false;
+//     break;
+//   }
+// }
+// console.log(isPal);
+
+//Bodlogo-12.
+
+// const N = 99;
+// let t = 0;
+
+// for (let i = 1; i <= N; i++) {
+//   if (t <= 2) {
+//     if (N % i === 0) {
+//       t++;
+//       console.log(i);
+//     }
+//   }
+// }
+// if (t <= 2) {
+//   console.log("anhnii too", "-", true);
+// } else {
+//   console.log("anhnii too", "-", false);
+// }
+
+//Bodlogo-13A.
+
+// const N = 10;
+// let t = 0;
+
+// for (let j = 1; j <= N; j++) {
+//   for (let i = 1; i <= j; i++) {
+//     if (t <= 2) {
+//       if (N % i === 0) {
+//         t++;
+//         // console.log(i);
+//       }
+//     }
+//   }
+//   if (t <= 2) {
+//     console.log("anhnii too", "=", j);
+//   }
+// }
+
+//Bodlogo-13B.
+
+// const N = 13;
+
+// let t = 0;
+
+// for (let i = 1; i <= N; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     if (i % j === 0) {
+//       t++;
+//       if (t > 2) {
+//         break;
+//       }
+//     }
+//   }
+// }
+
+//Bodlogo-14.
+
+// const N = 10;
+// const fibonach = [0, 1];
+
+// for (let i = 0; i <= N - 2; i++) {
+//   const an = fibonach[i] + fibonach[i + 1];
+//   fibonach.push(an);
+// }
+// console.log(fibonach);
+
+//Bodlogo-15.
+
+// const array = [1, 2, 2, 3, 4, 3];
+// const duplicate = [];
+
+// for (let i = 0; i <= array.length - 1; i++) {
+//   //asuuh??????
+//   if (!duplicate.includes(array[i])) {
+//     duplicate.push(array[i]);
+//   }
+// }
+// console.log("duplicate", "=", duplicate);
+
+//Bodlogo-16.
+
+// const d = 0;
+// const daraalal = [1, 2, 3, 4, 5, 6];
+
+// for (let i = 0; i <= daraalal.length - 1; i++) {
+//   const d = daraalal[i + 1] - daraalal[i];
+//   if (d !== 1) {
+//     console.log("alga bolson too", "=", daraalal[i] + 1);
+//     break;
+//   }
+// }
+
+//Bodlogo-18.
+
+// let a = 24;
+
+// let XIEX = 1;
+
+// for (let i = 2; i <= 9; i++) {
+//   if (a % i === 0 && a == 1) {
+//     a = a / i;
+//     console.log(a);
+//   }
+// }
+
+//Bodlogo-18B.
+
+let a = 24;
+
+while (let i = 2; i <= 9; i++) {
+  if (a % i === 0 && a == 1) {
+    a = a / i;
+    console.log(a);
   }
 }
-console.log(isPal);
