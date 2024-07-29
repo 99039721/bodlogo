471076c653cd687f9566f2610392aecb0f45fd5f
24lp3471@nest.edu.mn <24LP3471@G304-CC0117.local>
2024-07-29 12:57:42 +0800

    07.29

--- Todo.js
+++ Todo.js
@@ -1,32 +1,73 @@
-// const boards = document.querySelectorAll(".board");
+// const board = document.getElementById("board");
+// const huvilah = document.getElementById("huvilah");
+// const shinegarchig = ["To do", "In progress", "Stuck", "Done"];
+// const hooson = [];
+// const todo = document.getElementById("todo");
 
-// const todoElement = document.querySelector("#todo");
-// const inProgressElement = document.querySelector("#inprogress");
-// const stuckElement = document.querySelector("#stuck");
-// const doneElement = document.querySelector("#done");
+// huvilah.addEventListener("click", function () {
+//   for (let i = 2; 1 <= 4; i++) {
+//     const huvilah = board.cloneNode(true);
+//     const heading = huvilah.querySelector("garchig");
+//     if (heading) {
+//       heading.textContent = `${i}`;
+//     }
 
-const garchigElement = document.querySelector("#garchig");
-const shinegarchig = ["To do", "In progress", "Stuck", "Done"];
-const hooson = [];
+//     board.parentNode.insertBefore(clonedDiv, OrginalDiv.nextSibling);
+//   }
+// });
 
-boards.forEach((shinegarchig) => {
-  // document.querySelector("#garchig").innerHTML = shinegarchig[i];
-  hooson.push(shinegarchig);
-  console.log(hooson);
+// const board = document.querySelector(".board");
+// const button = document.getElementById("add");
+
+// const taskCreator = (Garchig, Desc, Status, Priority) => {
+//   return `<div class="board-header">
+//             <div id="garchig">To do</div>
+//             <div class="count"></div>
+//           </div>
+//           <div class="cards">
+//             <div class="card"></div>
+//           </div>
+//           <div class="addcard">
+//             <i class="fa-solid fa-plus"></i>
+
+//             <button id="huvilah">Add card</button>`;
+// };
+// button.addEventListener("click", () => {
+//   const task = taskCreator("Geriin daalgavar", "hiigeerei", "To do", "Low");
+//   const baigaaYumnuud = board.innerHTML;
+//   board;
+// });
+
+const addCard = document.getElementById("addCard");
+const addCardBtn = document.querySelectorAll(".addCardBtn");
+
+addCardBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    addCard.style.display = "flex";
+  });
 });
 
-// console.log(boards);
+window.addEventListener("click", (event) => {
+  if (event.target == addCard) {
+    addCard.style.display = "none";
+  }
+});
 
-// const items = ["item1", "item2", "item3"];
-// const copyItems = [];
+const check = {
+  title: "",
+  description: "",
+  status: "",
+  priority: "",
+};
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const status = document.getElementById("status");
+const priority = document.getElementById("priority");
 
-// // before
-// // for (let i = 0; i < items.length; i++) {
-// //   copyItems.push(items[i]);
-// // }
+const addTaskBtn = document.querySelectorAll(".addTaskBtn");
 
-// // after
-// items.forEach((item) => {
-//   copyItems.push(item);
-// });
-// console.log(copyItems);
+addTaskBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    addCard.style.display = title.innerText;
+  });
+});
