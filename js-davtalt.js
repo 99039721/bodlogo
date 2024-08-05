ae27911f2980ba2696592f28ff359b6c84960326
24lp3471@nest.edu.mn <24LP3471@G304-CC0117.local>
2024-08-05 12:44:35 +0800

    08.05

---
+++ js-davtalt.js
@@ -0,0 +1,15 @@
+// const arr = (arr) => {
+//   arr.forEach((element) => {
+//     console.log(element);
+//   });
+// };
+
+// arr([1, 123456, 34]);
+
+const printArray = (arr) => {
+  arr.forEach((element) => {
+    console.log(element);
+  });
+};
+
+printArray([1, 2, 3, 4, 5]);
